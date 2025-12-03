eca55fc019fe7229b07214ee41d0cbd0c94feefe
isaac <192796241+kehinde2025@users.noreply.github.com>
2025-12-03 18:59:08 +0100

    Update Tailwind CSS content paths in config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./app/**/*.{js,ts,jsx,tsx}", // include app directory
+    "./pages/**/*.{js,ts,jsx,tsx}", // include pages (if any)
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
